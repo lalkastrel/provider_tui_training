e93d2a46e06cc9b651b0f0e5a3b75010373d6224
You <lalkastrel@gmail.com>
2025-12-18 14:06:47 +0300

    add functions to page3

--- src/app/components/page3-component/page3-component.ts
+++ src/app/components/page3-component/page3-component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { signal, computed, input} from '@angular/core';
+import {TuiTextfield} from '@taiga-ui/core';
+import {TuiTextarea} from '@taiga-ui/kit';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page3-component',
-  imports: [],
+  imports: [TuiTextarea, TuiTextfield, FormsModule],
   templateUrl: './page3-component.html',
   styleUrl: './page3-component.less',
 })
 export class Page3Component {
+  aValue = signal<string>('1');
+  bValue = signal<string>('1');
 
+  pow_res = computed(() => {
+    const a = +this.aValue() || 0;
+    const b = +this.bValue() || 0;
+    return a ** b;
+  })
 }
