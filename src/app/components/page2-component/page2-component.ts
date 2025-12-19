cda04fe6adfee44ffa231586b9d2b07b23384d2e
You <lalkastrel@gmail.com>
2025-12-19 23:59:57 +0300

    change getNotes

--- src/app/components/page2-component/page2-component.ts
+++ src/app/components/page2-component/page2-component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
-import { Page2service } from '../../services/page2service/page2service';
+import { Component, signal } from '@angular/core';
+import { Note, Page2service } from '../../services/page2service/page2service';
+import { rxResource } from '@angular/core/rxjs-interop'
+import { catchError, EMPTY, Observable, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-page2-component',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './page2-component.html',
   styleUrl: './page2-component.less',
 })
 export class Page2Component {
   constructor(private noteService: Page2service) {}
+  notes$!: Observable<Note[]>
+
+  readonly isError = signal(false)
 
   ngOnInit() {
-    this.noteService.fetchNotes()
+    this.notes$ = this.noteService.getNotes().pipe(tap(() => {
+      this.isError.set(false)
+    }),
+  catchError(() => {
+      console.error(`Ошибка загрузки`); 
+      this.isError.set(true)
+      return EMPTY    
+    }))
   }
-
-  protected get notes() {
-    return this.noteService.getNotes()
-  }
-
-  protected get isError() {
-    return this.noteService.getError()
-  }; 
 }
--- src/app/services/page2service/page2service.ts
+++ src/app/services/page2service/page2service.ts
@@ -1,6 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { catchError, EMPTY } from 'rxjs';
 
 export interface Note {
   id: number,
@@ -13,29 +14,32 @@ export interface Note {
 })
 export class Page2service {
   private readonly apiUrl = environment.apiUrl + '/notes'
-  private notes = signal<Note[]>([])
-  private isError = signal<Boolean>(false)
+  // private notes = signal<Note[]>([])
+  // private isError = signal<Boolean>(false)
 
   constructor(private http: HttpClient) {}
-
-  fetchNotes() {
-    this.http.get<Note[]>(this.apiUrl).subscribe({
-      next: (data) => {
-        this.notes.set(data)
-        this.isError.set(false)
-      },
-      error: (error) => {
-        console.error(`Ошибка загрузки метод по адресу ${this.apiUrl}`);
-        this.notes.set([])
-        this.isError.set(true)
-      }
-    });
-  }
-
   getNotes() {
-    return this.notes
-  } 
-  getError() {
-    return this.isError
+    return this.http.get<Note[]>(this.apiUrl)
   }
+
+  // fetchNotes() {
+  //   this.http.get<Note[]>(this.apiUrl).subscribe({
+  //     next: (data) => {
+  //       this.notes.set(data)
+  //       this.isError.set(false)
+  //     },
+  //     error: (error) => {
+  //       console.error(`Ошибка загрузки метод по адресу ${this.apiUrl}`);
+  //       this.notes.set([])
+  //       this.isError.set(true)
+  //     }
+  //   });
+  // }
+
+  // getNotes() {
+  //   return this.notes
+  // } 
+  // getError() {
+  //   return this.isError
+  // }
 }
